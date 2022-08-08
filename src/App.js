67192d57460cc5e387c491865509f10b1f9b8d06
HodRZ <hodaifa.radwan@gmail.com>
2022-08-08 15:24:03 +0300

    Add Feature authentication

--- src/App.js
+++ src/App.js
@@ -8,19 +8,36 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import Profile from './Profile';
+import { withAuth0 } from '@auth0/auth0-react';
+import Welcome from './Welcome';
+
+
 
 class App extends React.Component {
   render() {
+    const { isAuthenticated } = this.props.auth0;
     return (
       <>
         <Router>
           <Header />
           <Routes>
-            <Route 
+            {isAuthenticated && <Route
               exact path="/"
               element={<BestBooks />}
             >
+            </Route>}
+            {!isAuthenticated && <Route
+              exact path="/"
+              element={<Welcome />}
+            >
+            </Route>}
+            <Route
+              exact path="/profile"
+              element={<Profile />}
+            >
             </Route>
+
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
           </Routes>
           <Footer />
@@ -30,4 +47,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0(App);
